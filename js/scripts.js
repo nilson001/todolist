e57fb10a99d4ed189944fbf8d54e48785a705382
Nilson001 <risonilson69@gmail.com>
2023-08-09 10:26:18 -0400

    add done n remove task js

--- js/scripts.js
+++ js/scripts.js
@@ -48,3 +48,18 @@ todoform.addEventListener("submit", (e) => {
     saveTodo(inputValue);
   }
 });
+
+document.addEventListener("click", (e) => {
+  const targetEl = e.target;
+  const parentEl = targetEl.closest("div");
+
+  if (targetEl.classList.contains("finish-todo")) {
+    parentEl.classList.toggle("done");
+  }
+  if (targetEl.classList.contains("remove-todo")) {
+    parentEl.remove();
+  }
+  if (targetEl.classList.contains("edit-todo")) {
+    console.log("Clicou em editar");
+  }
+});
